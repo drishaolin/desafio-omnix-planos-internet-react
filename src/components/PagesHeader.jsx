c5304202a9dc2c9fa7662281c13c5591c392dd87
Adriana Mucciolo <drishaolin@gmail.com>
2021-10-29 15:38:52 -0300

    remove styles from PageHeader component

--- src/components/PagesHeader.jsx
+++ src/components/PagesHeader.jsx
@@ -1,13 +1,4 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-    background-color: #140c4480;
-`;
-const MainTitle = styled.h1`
-    font-size: ${(props) => props.size};
-    text-align: center;
-    color: #ffeb3b;
-`;
+import { Container, MainTitle } from "../styles";
 
 export default function PagesHeader() {
     return (
